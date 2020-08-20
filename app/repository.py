164e2e5877a40c9f2119600ff7aaf33d23a3b89e
Jeevan Pillay <jeevan.pillay8@gmail.com>
2020-08-20 15:49:30 +1000

    added repository type enum

--- app/repository.py
+++ app/repository.py
@@ -1,12 +1,29 @@
 # repository.py
 from google.cloud import bigquery
+from enum import IntEnum
+
+
+class RepositoryEnum(IntEnum):
+    SQL = 0
+    NO_SQL = 1
+
+    @staticmethod
+    def to_char(a: int):
+        return {
+            0: 'SQL Database',
+            1: 'NoSQL Database',
+        }[a]
 
 
 class Repository:
     def __init__(self):
         self.client = self._setup_client()
+        self.type = self._setup_type()
 
     def _setup_client(self):
+        pass
+
+    def _setup_type(self):
         pass
 
 
@@ -16,6 +33,9 @@ class RepositoryBigQuery(Repository):
 
     def _setup_client(self):
         return bigquery.Client()
+
+    def _setup_type(self):
+        return RepositoryEnum.SQL
 
     def query(self, query):
         return self.client.query(query)

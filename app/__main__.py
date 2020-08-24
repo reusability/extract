c23d5c7cc80fa2aca888b6266224227cd247df30
Jeevan Pillay <jeevan.pillay8@gmail.com>
2020-08-24 20:31:45 +1000

    update todos

--- app/__main__.py
+++ app/__main__.py
@@ -2,21 +2,21 @@
 from .helpers import AppBigQueryStorage
 
 # todos
-# pre-mining
+# pre-mining -- see branch: feature/mining
 # 1. create yaml with mapped github repositories and associated projects
 # 2. utils/yaml.py -- should pull and push data into mining algorithm
 #
-# data mining -> data wrangling
+# data mining -> data wrangling -- see branch: feature/mining
 # 1. sql query script to fetch a single project
 # 2. remove unwanted files (wrangling) -- keep only .java but should be extendable to more files
 # 3. iterate query script to fetch various projects
 #
-# data processing (metrics analysis)
+# data processing (metrics analysis) -- see branch: feature/runner-ck
 # 1. once fetch a project, run CK metrics on the files
 # 2. clean output and push to csv file
 # 3. iterate multiple projects
 #
-# data post-processing (aws integration for on-line usage)
+# data post-processing (aws integration for on-line usage) -- see branch: env/cd-aws-s3
 # 1. push csv data into an S3 object
 # 2. /research repository should pull the S3 object
 #

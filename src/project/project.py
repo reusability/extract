c6eaba2e55436e0d8590cf79e4c9e3eb2752b346
ahmed <ahmed.laseefr96@gmail.com>
2020-09-14 23:48:40 +0800

    added script to get maven usage of each release

--- src/project/project.py
+++ src/project/project.py
@@ -3,6 +3,9 @@ from dataclasses import dataclass
 from pathlib import Path
 from src.utils import Subprocess
 from src.utils import make_dir
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
 
 class Project:
@@ -37,6 +40,54 @@ class Project:
 
         # run the clone command
         self.subprocess.Run()
+
+        # run maven_reuse
+        self._maven_reuse()
+
+    def _maven_reuse(self):
+        page = requests.get(self.maven)
+
+        # load html page
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        data = []
+        table = soup.find("table", attrs={"class": "grid versions"})
+        table_body = table.find_all("tbody")
+
+        # loop over the releases table: for each major release
+        for t in table_body:
+            rows = t.find_all("tr")
+            # for each minor release
+            for row in rows:
+                cols = row.find_all("td")
+                minor_releases = []
+                # for each cell
+                for element in cols:
+                    cell_data = element.text.strip()
+                    try:
+                        # 'rowspan' --> means this cell is for major release. e.g 4.X.X
+                        data.append(["main", cell_data, int(element["rowspan"]) - 1])
+                    except Exception as error:  # noqa : F841
+                        # if the cell is not 'rowspan', it means it stores the actual release. e.g 4.2.1
+                        minor_releases.append(cell_data)
+                data.append(minor_releases)
+
+        # write the data to .csv file
+        # TODO: move this as a subprocess
+        data_frame = {"version": [], "usage": []}
+        for d in data:
+            if len(d) == 4:
+                data_frame["version"].append(d[0])
+                data_frame["usage"].append(d[2])
+                # TODO: store the version number so it can be used to checkout in github
+
+        df = pd.DataFrame(data_frame)
+        # .csv format: release number, usage
+        df.to_csv(
+            "{}/{}_maven_reuse.csv".format(self.output_directory, self.name),
+            index=False,
+            header=False,
+        )
 
     @staticmethod
     def make_dir(path):
